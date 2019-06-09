42818cdaed42c2be350168993e1992af8660c964
Matthew DeVore <matvore@gmail.com>
2019-06-09 09:12:19 -0700

    定義されていない関数の宣言を消す。

--- src/romazi.h
+++ src/romazi.h
@@ -9,7 +9,6 @@ struct kanji_data {
 
 extern const char KEY_INDEX_TO_CHAR_MAP[MAPPABLE_CHAR_COUNT];
 
-char key_index_to_char(size_t index);
 size_t char_to_key_index(char ch);
 
 struct unused_kanji_keys {
