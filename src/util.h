36423fae210d50ea8044a7cb0901cfb528fea0e5
Matthew DeVore <matvore@gmail.com>
2019-06-27 09:31:25 -0700

    nusort: QSORTの定義のインデントを減して、より読み易くする

--- src/util.h
+++ src/util.h
@@ -21,27 +21,26 @@ static inline void qsort_push_frame(
 }
 
 #define QSORT(p, el, cnt, lt) \
-	do { \
-		struct qsort_frames f; \
-		char pivot[sizeof(*(el))]; \
-		f.f = f.end = xcalloc(cnt / 2, sizeof(*f.f)); \
-		qsort_push_frame(&f, 0, cnt); \
-		while (f.end != f.f) { \
-			size_t begin, p##a, p##b; \
-			f.end--; \
-			p##b = begin = f.end->begin; \
-			memcpy(pivot, el + begin, sizeof(*(el))); \
-			for (p##a = begin + 1; p##a < f.end->end; p##a++) { \
-				if (lt) { \
-					(el)[p##b] = (el)[p##a]; \
-					p##b++; \
-					(el)[p##a] = (el)[p##b]; \
-					memcpy(&p##b[el], pivot, \
-					       sizeof(*(el))); \
-				} \
+do { \
+	struct qsort_frames f; \
+	char pivot[sizeof(*(el))]; \
+	f.f = f.end = xcalloc(cnt / 2, sizeof(*f.f)); \
+	qsort_push_frame(&f, 0, cnt); \
+	while (f.end != f.f) { \
+		size_t begin, p##a, p##b; \
+		f.end--; \
+		p##b = begin = f.end->begin; \
+		memcpy(pivot, el + begin, sizeof(*(el))); \
+		for (p##a = begin + 1; p##a < f.end->end; p##a++) { \
+			if (lt) { \
+				(el)[p##b] = (el)[p##a]; \
+				p##b++; \
+				(el)[p##a] = (el)[p##b]; \
+				memcpy(&p##b[el], pivot, sizeof(*(el))); \
 			} \
-			qsort_push_frame(&f, begin, p##b); \
-			qsort_push_frame(&f, p##b + 1, p##a); \
 		} \
-		free(f.f); \
-	} while(0)
+		qsort_push_frame(&f, begin, p##b); \
+		qsort_push_frame(&f, p##b + 1, p##a); \
+	} \
+	free(f.f); \
+} while(0)
