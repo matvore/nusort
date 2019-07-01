d9b225d77eee19ce8d7ae83aa6b72a72cc1da169
Matthew DeVore <matvore@gmail.com>
2019-07-01 13:21:09 -0700

    nusort: dump_rad_soのC実装を書いてみた

--- src/check_kanji_db_order.c
+++ src/check_kanji_db_order.c
@@ -3,18 +3,105 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "util.h"
+
+static const char *ADOBE_JAPAN = "\tkRSAdobe_Japan1_6";
+
+struct sort_key {
+	unsigned rad: 8;
+	unsigned strokes: 8;
+};
+
+#define MAX_K 15
+
+struct sort_info {
+	char c[5];
+	struct sort_key k[MAX_K];
+};
+
+static struct sort_info sort_infos[20000];
+static size_t sort_infos_nr;
+
+void set_utf8_byte(char *d, unsigned char c) { *d = 0x80 | (c & 0x3f); }
+
+static void decode_codepoint(char *d, const char *codepoint_str)
+{
+	long codepoint = strtol(codepoint_str, NULL, 16);
+	if (codepoint < 0x0800) {
+		fprintf(stderr, "コードポイントが対象外: %ld\n", codepoint);
+		exit(20);
+	}
+	if (codepoint <= 0xffff) {
+		*d++ = 0xe0 | (codepoint >> 12);
+	} else {
+		*d++ = 0xf0 | (codepoint >> 18);
+		set_utf8_byte(d++, (unsigned char)(codepoint >> 12));
+	}
+	set_utf8_byte(d++, (unsigned char)(codepoint >> 6));
+	set_utf8_byte(d++, (unsigned char)codepoint);
+	*d = 0;
+}
+
 static int process_rad_so_line(const char *line)
 {
-	const char *curs = line;
-	if (curs[0] == '#' || curs[0] == '\n')
+	char codepoint_str[6];
+	int prefix_len;
+	const char *cur;
+	struct sort_key *k = sort_infos[sort_infos_nr].k;
+	size_t k_nr;
+
+	if (line[0] == '#' || line[0] == '\n')
 		return 0;
-	if (strncmp("U+", curs, 2)) {
+	switch (sscanf(line, "U+%5[0-9A-F]%n", codepoint_str, &prefix_len)) {
+	case 0:
 		fprintf(stderr,
 			"警告: 行の形式が間違っています：%s\n",
-			curs);
+			line);
 		return 0;
+	case 1:
+		break;
+	default:
+		fprintf(stderr, "予期していないエラー: %s\n",
+			strerror(errno));
+		return 10;
 	}
-	curs += 2;
+
+	cur = line + prefix_len;
+	if (strncmp(ADOBE_JAPAN, cur, strlen(ADOBE_JAPAN)))
+		return 0;
+
+	cur += strlen(ADOBE_JAPAN);
+	while (1) {
+		int rad, so, len;
+		if (!*cur || *cur == '\n')
+			break;
+
+		switch (sscanf(cur, " %*1[CV]+%*d+%d.%*d.%d%n",
+			       &rad, &so, &len)) {
+		case 0:
+		case 1:
+			fprintf(stderr, "行の形式が間違っています: (列 %d)\n%s",
+				(int)(cur - line), line);
+			return 11;
+		case 2:
+			break;
+		default:
+			fprintf(stderr, "予期していないエラー: %s\n",
+				strerror(errno));
+			return 12;
+		}
+		cur += len;
+		if (k_nr == MAX_K) {
+			fprintf(stderr, "並べ替えキーが多すぎます：%s", line);
+			return 13;
+		}
+		k->rad = rad;
+		k->strokes = so;
+		k_nr++;
+	}
+
+	decode_codepoint(sort_infos[sort_infos_nr++].c, codepoint_str);
+
 	return 0;
 }
 
@@ -53,5 +140,9 @@ int check_kanji_db_order(void)
 			"WARNING: did not close %s properly\n:\t%s\n",
 			rad_so_db_path, strerror(errno));
 	}
+
+	QSORT(, sort_infos, sort_infos_nr,
+	      strcmp(sort_infos[a].c, sort_infos[b].c));
+	fprintf(stderr, "%ld字の並べ替えキーを読み込み済み\n", sort_infos_nr);
 	return res;
 }
