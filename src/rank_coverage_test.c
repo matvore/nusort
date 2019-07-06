30f8cdf40a73bff6b3b1f871e09869abad28747c
Matthew DeVore <matvore@gmail.com>
2019-07-06 16:41:29 -0700

    nusort: rank_coverage_testにテストをもう一つ追加

--- src/rank_coverage_test.c
+++ src/rank_coverage_test.c
@@ -18,5 +18,14 @@ int main(void)
 	}
 	end_test("0");
 
+	start_test(__FILE__, "half_fits");
+	{
+		size_t i;
+		rank_coverage_reset(2, 2);
+		for (i = 1; i <= 4; i++)
+			fprintf(out, "%d ", rank_coverage_add_kanji(i));
+	}
+	end_test("32767 0 0 0 ");
+
 	return 0;
 }
