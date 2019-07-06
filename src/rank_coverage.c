cba4b843ce6b2bae47ce9a50911f397fdfe0879e
Matthew DeVore <matvore@gmail.com>
2019-07-05 21:59:49 -0700

    nusort: rank_coverageモジュールを書き始める

---
+++ src/rank_coverage.c
@@ -0,0 +1,10 @@
+#include "rank_coverage.h"
+
+void rank_coverage_reset(int target_rank, int key_count)
+{
+}
+
+int rank_coverage_add_kanji(int rank)
+{
+	return 0;
+}
---
+++ src/rank_coverage.h
@@ -0,0 +1,2 @@
+void rank_coverage_reset(int target_rank, int key_count);
+int rank_coverage_add_kanji(int rank);
