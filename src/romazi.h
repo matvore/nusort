3a9d17a02978d388c74ebdaea2aeadd7d87dc166
Matthew DeVore <matvore@gmail.com>
2019-09-16 06:08:43 -0700

    使われていないストラクト宣言を消す。

--- src/romazi.h
+++ src/romazi.h
@@ -3,10 +3,6 @@
 #define KANJI_KEY_COUNT 40
 #define MAPPABLE_CHAR_COUNT 82
 
-struct kanji_data {
-	int entry[MAPPABLE_CHAR_COUNT * MAPPABLE_CHAR_COUNT];
-};
-
 extern const char KEY_INDEX_TO_CHAR_MAP[MAPPABLE_CHAR_COUNT];
 
 size_t char_to_key_index(char ch);
