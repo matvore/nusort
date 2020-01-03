9b7d2b9b2f2ee62ebc2f517e4ffb531310103e25
Matthew DeVore <matvore@gmail.com>
2020-01-03 07:48:41 -0800

    code_cmp 関数に説明を追加する

--- src/mapping.h
+++ src/mapping.h
@@ -9,7 +9,14 @@ struct mapping {
 	unsigned include_kanji : 1;
 };
 
+/*
+ * ２つの入力コードを比較します。
+ *
+ * 先に長さを比較して、異なっていれば長い方を大きく標価します。
+ * 長さが同じ時は strcmp でコードポイントを比較します。
+ */
 int code_cmp(char const *a, char const *b);
+
 void mapping_populate(struct mapping *mapping);
 void mapping_destroy(struct mapping *mapping);
 
