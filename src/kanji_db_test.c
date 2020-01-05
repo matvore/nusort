f5bc062a31245139b8db8b54328c4e7159898ab5
Matthew DeVore <matvore@gmail.com>
2020-01-05 15:16:14 -0800

    区切り字を「槻」から「標」に代える

--- src/kanji_db_test.c
+++ src/kanji_db_test.c
@@ -3,12 +3,18 @@
 #include "test_util.h"
 #include "util.h"
 
-static void print_if_cutoff(char const *k)
+static int cutoff_type(char const *k)
 {
 	struct kanji_entry const *e = kanji_db_lookup(k);
-	if (!e)
-		xfprintf(err, "見つからない: %s\n", k);
-	if (e->cutoff_type)
+	if (e)
+		return e->cutoff_type;
+	xfprintf(err, "見つからない: %s\n", k);
+	return 0;
+}
+
+static void print_if_cutoff(char const *k)
+{
+	if (cutoff_type(k))
 		xfprintf(out, "%s\n", k);
 }
 
@@ -41,4 +47,32 @@ int main(void)
 		print_if_cutoff("前");
 	}
 	end_test("前\n");
+
+	start_test("tree_rad_with_11_resid_strokes_bad_cutoffs");
+	{
+		print_if_cutoff("槻");
+		print_if_cutoff("槽");
+		print_if_cutoff("槿");
+		print_if_cutoff("樂");
+		print_if_cutoff("樅");
+		print_if_cutoff("樋");
+		print_if_cutoff("樓");
+		print_if_cutoff("樗");
+		print_if_cutoff("樟");
+		print_if_cutoff("樣");
+	}
+	end_test("");
+
+	start_test("tree_rad_with_11_resid_strokes_good_cutoffs");
+	{
+		int cutoff_count = 0;
+		if (cutoff_type("標"))
+			cutoff_count++;
+		if (cutoff_type("権"))
+			cutoff_count++;
+		if (cutoff_type("横"))
+			cutoff_count++;
+		xfprintf(out, "%d", cutoff_count);
+	}
+	end_test("1");
 }
