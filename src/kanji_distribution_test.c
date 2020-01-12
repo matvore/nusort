56d88602709cefd1e1f38e3066ecb6e014e81752
Matthew DeVore <matvore@gmail.com>
2020-01-11 21:29:17 -0800

    kanji_db.c: 区切り字のいくつかを正す

--- src/kanji_distribution_test.c
+++ src/kanji_distribution_test.c
@@ -52,6 +52,25 @@ int main(void)
 		DESTROY_ARRAY(romazi_m);
 	}
 	end_test("");
+
+	start_test("does_not_use_uncommon_character_for_cutoff_no_kana");
+	{
+		int key;
+
+		struct kanji_distribution kd = {0};
+		struct key_mapping_array m = {0};
+
+		kanji_distribution_set_preexisting_convs(&kd, &m);
+		kanji_distribution_auto_pick_cutoff(&kd);
+		kanji_distribution_populate(&kd);
+
+		for (key = 0; key < kd.line_stats_nr; key++)
+			validate_line(kd.line_stats + key);
+
+		kanji_distribution_destroy(&kd);
+	}
+	end_test("");
+
 	start_test("can_generate_distribution_with_only_kanji");
 	{
 		struct kanji_distribution kd = {0};
