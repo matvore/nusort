6f1d03e24afc860803ac177547ab281b604db0cc
Matthew DeVore <me@warehouse13>
2022-04-30 17:46:28 -0700

    コマンド名を処理するコードを書き直す
    ダブっているコードを整理して、これからのコマンド追加
    をらくにしました。

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "commands.h"
 #include "streams.h"
+#include "util.h"
 
 #ifdef _MSC_VER
 #include <fcntl.h>
@@ -10,23 +11,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-static const char *USAGE =
-"Usage:\n"
-"\tnusort check_kanji_db_order [--db-out] [-q] [--residual-stroke-counts]\n"
-"\tnusort free_kanji_keys\n"
-"\tnusort h2k\n"
-"\tnusort input\n"
-"\tnusort kana_stats\n"
-"\tnusort kanji_db_chart [漢字数]\n"
-"\tnusort last_rank_contained [-c] [-s] [-k] [-n] "
-	"CUTOFF_KANJI_1..CUTOFF_KANJI_n\n"
-"\tnusort longest_rsc_block\n"
-"\tnusort make_map\n"
-"\tnusort rsc_sort_key\n"
-;
+static const char *binname;
+
+static int input_cmd(char const *const *argv, int argc)
+{
+	return input(argv, argc, /*set_raw_mode=*/1);
+}
+
+static const struct {
+	int (*impl)(char const *const *argv, int argc);
+
+	char const *name, *usage;
+} commands[] = {
+	{&free_kanji_keys, "free_kanji_keys", ""},
+	{&h2k, "h2k", ""},
+	{&input_cmd, "input", ""},
+	{&kana_stats, "kana_stats", ""},
+	{&kanji_db_chart, "kanji_db_chart", " [漢字数]"},
+	{&check_kanji_db_order, "check_kanji_db_order", " [--db-out] [-q] [--residual-stroke-counts]"},
+	{&print_last_rank_contained, "last_rank_contained", " [-c] [-s] [-k] [-n] CUTOFF_KANJI_1..CUTOFF_KANJI_n"},
+	{&longest_rsc_block, "longest_rsc_block", ""},
+	{&make_map, "make_map", ""},
+	{&rsc_sort_key, "rsc_sort_key", ""},
+	{0},
+};
+
+static void usage(FILE *stream)
+{
+	int ci;
+
+	fprintf(stream, "USAGE: %s <command> <args...>\n", binname);
+
+	fputs("COMMANDS:\n", stream);
+
+	for (ci = 0; commands[ci].impl; ci++)
+		fprintf(stream, "\t%s%s\n", commands[ci].name, commands[ci].usage);
+
+	fputc('\n', stream);
+}
 
 int main(int argc, const char **argv)
 {
+	int ci;
+
 	err = stderr;
 	out = stdout;
 	in = stdin;
@@ -40,26 +67,27 @@ int main(int argc, const char **argv)
 	setmode(fileno(stderr), O_BINARY);
 #endif
 
-	if (argc >= 2 && !strcmp(argv[1], "free_kanji_keys"))
-		return free_kanji_keys(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "h2k"))
-		return h2k(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "input"))
-		return input(argv + 2, argc - 2, /*set_raw_mode=*/1);
-	if (argc >= 2 && !strcmp(argv[1], "kana_stats"))
-		return kana_stats(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "kanji_db_chart"))
-		return kanji_db_chart(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "check_kanji_db_order"))
-		return check_kanji_db_order(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "last_rank_contained"))
-		return print_last_rank_contained(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "longest_rsc_block"))
-		return longest_rsc_block(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "make_map"))
-		return make_map(argv + 2, argc - 2);
-	if (argc >= 2 && !strcmp(argv[1], "rsc_sort_key"))
-		return rsc_sort_key(argv + 2, argc - 2);
-	fprintf(stderr, "%s", USAGE);
-	return 1;
+	if (argc < 1) DIE(0, "argc: %d", argc);
+
+	binname = *argv++;
+	argc--;
+
+	if (!argc
+		|| !strcmp(*argv, "--help")
+		|| !strcmp(*argv, "-h")
+		|| !strcmp(*argv, "help")
+		|| !strcmp(*argv, "-help")
+	) {
+		usage(stdout);
+		exit(0);
+	}
+
+	for (ci = 0; commands[ci].impl; ci++) {
+		if (!strcmp(commands[ci].name, *argv))
+			exit(commands[ci].impl(argv+1, argc-1));
+	}
+
+	usage(stderr);
+
+	exit(1);
 }
