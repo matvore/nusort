dcca9a1110280aa270b6e23c513a523e627bcd06
Matthew DeVore <matvore@pobox.com>
2025-07-10 16:49:13 +0000

    kanji_db.c: 87 字を加わえる

--- src/check_kanji_db_order_test.c
+++ src/check_kanji_db_order_test.c
@@ -10,7 +10,7 @@ int main(void)
 	set_test_source_file(__FILE__);
 
 	while (run_test("check_kanji_db_order_test_quiet",
-			"13696字の並べ替えキーを読み込み済み\n"
+			"13712字の並べ替えキーを読み込み済み\n"
 			"exit: 0\n")) {
 		const char *argv[] = {"-q"};
 		int exit = check_kanji_db_order(argv, 1);
