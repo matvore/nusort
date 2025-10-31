dbb8629fecc3e9b54cf0b84c89faf19675b94ebe
Matthew DeVore <matvore@pobox.com>
2025-10-31 14:30:41 +0000

    「雋」を含む字、部首外画数13画の字を導入
    「雋」の下部の画数を5画とします。
    
    部首外画数=13で高優先度の字を追加します。

--- check_kanji_db_order_test.c
+++ check_kanji_db_order_test.c
@@ -13,7 +13,7 @@ int main(void)
 	set_test_source_file(__FILE__);
 
 	while (run_test("check_kanji_db_order_test_quiet",
-			"14135字の並べ替えキーを読み込み済み\n"
+			"14136字の並べ替えキーを読み込み済み\n"
 			"exit: 0\n")) {
 		char *argv[] = {"-q"};
 		memset(&fs, 0, sizeof fs);
